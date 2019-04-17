1869952358b4b829e893c2c3182f471fece7c803
Diego Fredes <diego.fredes@externos.bci.cl>
2019-04-17 19:35:50 -0400

    se agrega login controller y entities

---
+++ src/main/java/com/duoc/serviexpress/ms/entity/request/LoginUserRequest.java
@@ -0,0 +1,23 @@
+package com.duoc.serviexpress.ms.entity.request;
+
+
+public class LoginUserRequest {
+    String usuario;
+    String contraseña;
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+}
